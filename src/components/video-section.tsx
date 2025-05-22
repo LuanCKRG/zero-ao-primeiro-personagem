da9d4ed07ae39f92d193dad77a0c3dbf87795c14
LuanCKRG <luancamposck@gmail.com>
2025-05-22 15:42:28 -0300

    style(video-section): adjust to improve responsive

--- src/components/video-section.tsx
+++ src/components/video-section.tsx
@@ -2,7 +2,7 @@
 
 export const VideoSection = () => (
 	<iframe
-		className="aspect-video w-1/2"
+		className="aspect-video w-full rounded-lg"
 		src="https://www.youtube.com/embed/F94_J0Bij5E"
 		title="YouTube video player"
 		allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
