7efb366e4916b8cf5486073c1a0b88faf7740ce1
LuanCKRG <luancamposck@gmail.com>
2025-05-22 02:37:08 -0300

    feat(video-section): create VideoSection component with embedded YouTube video

---
+++ src/components/video-section.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+export const VideoSection = () => (
+	<iframe
+		className="aspect-video w-1/2"
+		src="https://www.youtube.com/embed/F94_J0Bij5E"
+		title="YouTube video player"
+		allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+		allowFullScreen
+	/>
+)
